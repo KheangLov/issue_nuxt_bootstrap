1fe74023feb89ddb7bafab4a4d6b7c7a3689e103
kheanglov <lovsokheang@gmail.com>
2022-03-09 17:42:49 +0700

    update pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,7 @@ export default {
       cachingExtensions: '@/plugins/workbox-range-request.js',
       preCaching: [
         {
-          url: ''
+          url: 'https://issue-project.netlify.app/admin/dashboard'
         }
       ],
       runtimeCaching: [
